37d1082234ef198514a010f9c05ee6adc24566f2
Mikhail <mikhailgrigoriev92@gmail.com>
2021-09-18 23:56:40 -0400

    Add recursive fibonacci sequences #fibs_rec

--- recursion.rb
+++ recursion.rb
@@ -5,4 +5,9 @@ def fibs(number, fibonacci_array = [0, 1])
   fibonacci_array
 end 
 
-p fibs(10)
+def fibs_rec(number, fibonacci_array = [0, 1])
+  return fibonacci_array if fibonacci_array.length == (number + 1)
+  fibs_rec(number, fibonacci_array.push(fibonacci_array[-1] + fibonacci_array[-2]))
+end
+
+p fibs_rec(10)
