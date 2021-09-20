8358e8c7c6fe1da2ab1132dfe3ead83bffd3367f
Mikhail <mikhailgrigoriev92@gmail.com>
2021-09-19 22:10:48 -0400

    Write pseudocode for #merge_sort

--- recursion.rb
+++ recursion.rb
@@ -7,7 +7,21 @@ end
 
 def fibs_rec(number, fibonacci_array = [0, 1])
   return fibonacci_array if fibonacci_array.length == (number + 1)
+
   fibs_rec(number, fibonacci_array.push(fibonacci_array[-1] + fibonacci_array[-2]))
 end
 
-p fibs_rec(10)
+array_to_sort = [5,2,6,1,4,3]
+
+def merge_sort(array)
+  items_to_sort = array.length
+  if  items_to_sort <= 1 
+    #merge the two halves together and return result
+  else
+    #split the array in half
+    #merge_sort(left_half)
+    #merge_sort(right_half)
+  end
+end
+
+p merge_sort(array_to_sort)
