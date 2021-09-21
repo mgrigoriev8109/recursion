a69558c017d4d82ce4450429f17050c5071e857e
Mikhail <mikhailgrigoriev92@gmail.com>
2021-09-21 06:37:05 -0400

    Fix merge in merge_sort

--- recursion.rb
+++ recursion.rb
@@ -1,5 +1,3 @@
-require 'pry-byebug'
-
 def fibs(number, fibonacci_array = [0, 1])
   (2..number).each do 
     fibonacci_array.push(fibonacci_array[-1] + fibonacci_array[-2])
@@ -13,29 +11,30 @@ def fibs_rec(number, fibonacci_array = [0, 1])
   fibs_rec(number, fibonacci_array.push(fibonacci_array[-1] + fibonacci_array[-2]))
 end
 
-array_to_sort = [2,1]
-
-def merge_sort(current_array, sorted_array=[])
+def merge_sort(current_array)
   current_length = current_array.length
   if current_length == 1
-    return current_array
+    current_array
   else
     elements_in_half = (current_length / 2).floor
     left_half = current_array.take(elements_in_half)
     right_half = current_array.drop(elements_in_half)
     sorted_left = merge_sort(left_half)
     sorted_right = merge_sort(right_half)
-    merge(left_half, right_half)
+    merge(sorted_left, sorted_right)
   end
-  
 end
-#binding.pry
 
 def merge(left_half, right_half)
   sorted_array = []
-  left_half.each do |element|
-    if element < right_half[0]
-      sorted_array.push(left_half(element))
+  number_of_merges = (left_half.length + right_half.length)
+  number_of_merges.times do
+    if left_half[0].nil?
+      sorted_array.push(right_half.shift)
+    elsif right_half[0].nil?
+      sorted_array.push(left_half.shift)
+    elsif left_half[0] < right_half[0]
+      sorted_array.push(left_half.shift)
     else
       sorted_array.push(right_half.shift)
     end
@@ -44,4 +43,5 @@ def merge(left_half, right_half)
 end
 
 
+array_to_sort = [2,1,5,3,4]
 p merge_sort(array_to_sort)
