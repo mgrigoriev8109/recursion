534981878320a370be96a59ae39a59a34aab5ed1
Mikhail <mikhailgrigoriev92@gmail.com>
2021-09-21 00:30:09 -0400

    Fix pseudocode for proper merge and base case

--- recursion.rb
+++ recursion.rb
@@ -1,3 +1,5 @@
+require 'pry-byebug'
+
 def fibs(number, fibonacci_array = [0, 1])
   (2..number).each do 
     fibonacci_array.push(fibonacci_array[-1] + fibonacci_array[-2])
@@ -11,24 +13,22 @@ def fibs_rec(number, fibonacci_array = [0, 1])
   fibs_rec(number, fibonacci_array.push(fibonacci_array[-1] + fibonacci_array[-2]))
 end
 
-array_to_sort = [5,2,6,1,4,3]
+array_to_sort = [5,2,1,3,6,4]
 
-def merge_sort(current_array, array_to_compare=[])
-  items_to_sort = current_array.length
-  if items_to_sort <= 1 
-    if current_array[0] < array_to_compare[0]
-      return current_array[0]
-    else
-      return array_to_compare[0]
-    end
+def merge_sort(current_array)
+  current_length = current_array.length
+  if current_length == 1
+    return current_array
   else
-    elements_in_half = ((items_to_sort / 2).floor)
+    elements_in_half = (current_length / 2).floor
     left_half = current_array.take(elements_in_half)
     right_half = current_array.drop(elements_in_half)
-    merge_sort(left_half, right_half)
-    merge_sort(right_half, left_half)
+    sorted_left = merge_sort(left_half)
+    sorted_right = merge_sort(right_half)
+    #write pseudocode to merge(sorted_left, sorted_right)
   end
-  current_array
+  
 end
-
+#binding.pry
 p merge_sort(array_to_sort)
+
