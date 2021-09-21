5f272b6cfb5d32d0ae67cb01c824c60a1d94da49
Mikhail <mikhailgrigoriev92@gmail.com>
2021-09-21 06:13:54 -0400

    Add #merge functionality

--- recursion.rb
+++ recursion.rb
@@ -13,9 +13,9 @@ def fibs_rec(number, fibonacci_array = [0, 1])
   fibs_rec(number, fibonacci_array.push(fibonacci_array[-1] + fibonacci_array[-2]))
 end
 
-array_to_sort = [5,2,1,3,6,4]
+array_to_sort = [2,1]
 
-def merge_sort(current_array)
+def merge_sort(current_array, sorted_array=[])
   current_length = current_array.length
   if current_length == 1
     return current_array
@@ -25,10 +25,23 @@ def merge_sort(current_array)
     right_half = current_array.drop(elements_in_half)
     sorted_left = merge_sort(left_half)
     sorted_right = merge_sort(right_half)
-    #write pseudocode to merge(sorted_left, sorted_right)
+    merge(left_half, right_half)
   end
   
 end
 #binding.pry
-p merge_sort(array_to_sort)
 
+def merge(left_half, right_half)
+  sorted_array = []
+  left_half.each do |element|
+    if element < right_half[0]
+      sorted_array.push(left_half(element))
+    else
+      sorted_array.push(right_half.shift)
+    end
+  end
+  sorted_array
+end
+
+
+p merge_sort(array_to_sort)
